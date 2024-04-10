92f78b6400b52c273e2687f1ac44a2b3415d5416
kevin.faramia@thecamp.fr <kevin.faramia@thecamp.fr>
2024-04-10 16:44:39 +0200

    Service HEALTH for CI CD

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
 # syntax=docker/dockerfile:1
 FROM node:14
 # Argument pour le port, avec une valeur par défaut
-ARG PORT=3001
+ARG PORT=3003
 ENV PORT=${PORT}
 WORKDIR /app
 COPY package*.json ./
 COPY .env ./
 RUN npm install
-RUN npm install cors
 RUN npm install -g nodemon
-COPY . .
+COPY middleware.js ./
+
 EXPOSE ${PORT}
 # Start the app based on the NODE_ENV variable with a default value
 CMD [ "/bin/sh", "-c", "if [ ${NODE_ENV} = 'development' ]; then npm run dev; else npm start; fi" ]
--- middleware.js
+++ middleware.js
@@ -1,11 +1,40 @@
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT_MIDDLEWARE || 3000;
 
 app.get('/', (req, res) => {
   res.send('MIDDLEWARE IS UP');
+});
+
+// Function to test a single microservice
+async function testMicroservice(url) {
+  try {
+    const response = await axios.get(url);
+    return { url, status: 'up', statusCode: response.status };
+  } catch (error) {
+    return { url, status: 'down', statusCode: error.response ? error.response.status : 'N/A' };
+  }
+}
+
+// Route to test all microservices
+app.get('/build-validation', async (req, res) => {
+  const services = [
+    'http://article-service:3003/article',
+    'http://auth-service:3001/auth',
+    'http://user-service:3002/user',
+    'http://cart-service:3005/cart',
+    'http://delivery-service:3007/delivery',
+    'http://menu-service:3004/menu',
+    'http://order-service:3006/order'
+  ];
+
+  // Map through services and test each one
+  const results = await Promise.all(services.map(service => testMicroservice(service)));
+
+  // Send the response
+  res.json(results);
 });
 
 app.listen(PORT, () => {
